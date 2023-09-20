5cd13f45355a270e80791161e8be017935632994
3nsar <84131267+3nsar@users.noreply.github.com>
2023-09-20 16:56:22 +0200

    commit

--- cards-demo/src/App.jsx
+++ cards-demo/src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+ 
+  const [product, setProduct]= useState([])
+  const [count,setCount] = useState(1)
+
+  const url = `https://jsonplaceholder.typicode.com/users/${count}`
+
+  const add = ()=>{
+    setCount(prev => prev +1)
+  }
+
+  const sub = ()=>{
+    setCount(prev => prev -1)
+  }
+
+
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      const res = await axios.get(url)
+      console.log(res.data)
+      setProduct(res.data)
+    }
+    fetchData()
+  },[count])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container'>
+      
+       
+          <div className="card">
+              <h1>{product.name}</h1>
+              
+              <button onClick={add}>NEXT</button>
+              <button onClick={sub}>PREV</button>
+          </div>
+       
+    </div>
   )
 }
 
